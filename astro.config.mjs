ba7835152f22dadfb4e7438fd0b2c67492d0dce0
wanto-production <ikwansatria3974@gmail.com>
2024-12-25 15:28:11 +0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 import Autoimport from 'unplugin-auto-import/vite'
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  base:'/news-app/',
   output:"static",
   integrations: [tailwind(), vue({ appEntrypoint:'./src/app.ts' }), mdx()],
-  site:"https://wanto-production.github.io/news-app",
 
   vite:{
-    base:'/news-app/',
-    build:{
-      outDir:'./dist'
-    },
     plugins:[
       Autoimport({
         imports:["vue","@vueuse/core"],
