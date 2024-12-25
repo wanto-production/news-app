f1111a1b8fcef4768955dc1b415c37538314db28
wanto-production <ikwansatria3974@gmail.com>
2024-12-25 16:07:34 +0700

    initial commit

--- src/components/vue/Navbar.vue
+++ src/components/vue/Navbar.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
     const categories = [
-       "business",
-			"health",
-			"science",
-			"sports",
-			"technology"
+        "business",
+		"health",
+		"science",
+		"sports",
+		"technology"
     ];
 
     import Button from '../ui/button/Button.vue';
