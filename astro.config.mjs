ea3efc0b31a4f1b3e417bdbf39f1d711a0a2c7db
wanto-production <ikwansatria3974@gmail.com>
2024-12-25 15:01:12 +0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import Autoimport from 'unplugin-auto-import/vite'
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  output:"static",
+  integrations: [tailwind(), vue({ appEntrypoint:'./src/app.ts' }), mdx()],
+
+  vite:{
+    plugins:[
+      Autoimport({
+        imports:["vue","@vueuse/core"],
+        dts:'./.astro/import.d.ts'
+      }),
+    ],
+  }
+});
---
+++ src/components/vue/Navbar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    const categories = [
+        "business",
+        "entertainment",
+        "general",
+        "health",
+        "science",
+        "sports",
+        "technology"
+    ];
+
+    import Button from '../ui/button/Button.vue';
+</script>
+
+<template>
+    <header class=" w-full h-[60px] flex justify-between items-center px-2 fixed top-0 ">
+        <div class="text-xl font-bold bg-gradient-to-r from-[#4a90e2] to-[#8e44ad] bg-clip-text text-transparent">
+            ambaNews
+        </div>
+        <Button class=" bg-gradient-to-r from-[#4a90e2] to-[#8e44ad]  rounded text-white" variant="outline"><a href="/business">back</a></Button>
+    </header>
+    <header class="w-full fixed top-[60px] h-[30px] flex items-center justify-around flex-wrap px-3 gap-3">
+        <a v-for="cat,i in categories" :key="i" :href="`/${cat}`" class="text-white " >
+            {{  cat  }}
+        </a>
+    </header>   
+    <div class="w-full fixed h-[3px] bg-gradient-to-r from-[#4a90e2] to-[#8e44ad] top-[140px] min-[526px]:top-[100px] " />
+</template>
